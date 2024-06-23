ceec9bf0632c6de8206a9eefadbd59306cf74981
Diego Medina <romeomdna@outlook.com>
2024-06-23 09:35:54 -0400

    social media image edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,31 @@
-import type { Metadata } from "next";
+import type  Metadata  from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider} from "./provider";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700", "900"] });
 
+export type Metadata = {
+  title: string;
+  description: string;
+  icons: {
+    icon: string;
+  };
+  image?: {
+    source: string;
+  };
+};
+
 export const metadata: Metadata = {
   title: "Lucian's Portfolio",
   description: "A modern E-Portfolio",
   icons: {
     icon: "/favicon.ico",
-  }
-};
+  },
+  image: {
+    source: "/opengraph-image.png",
+  },
+} as Metadata;
 
 export default function RootLayout({
   children,
